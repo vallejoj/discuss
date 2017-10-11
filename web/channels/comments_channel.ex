573a71ce63c9a856a77277644cd8bb8bfec99ff8
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-10-11 13:25:37 -1000

    about half through building out the channels and about to start creating a way to see our data through sockets on the web page.

--- web/channels/comments_channel.ex
+++ web/channels/comments_channel.ex
@@ -1,12 +1,16 @@
 defmodule Discuss.CommentsChannel do
-    use Discuss.Web :channel
+    use Discuss.Web, :channel
 
 
-    def join() do
-        
+    def join(name, _params, socket) do
+     
+        {:ok, %{hey: "there"}, socket}
     end
 
-    def handle_in() do
+    def handle_in(name, message, socket) do #is about any follow up communication
+        IO.puts("++++")
+        IO.puts(name)
         
+        {:reply, :ok, socket}
     end
 end
--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -6,9 +6,14 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 
 socket.connect()
 
-let channel = socket.channel("topic:subtopic", {})
+let channel = socket.channel("comments:1", {})
 channel.join()
   .receive("ok", resp => { console.log("Joined successfully", resp) })
   .receive("error", resp => { console.log("Unable to join", resp) })
+
+  document.querySelector('button').addEventListener('click', function() {
+    channel.push('comment:hello', {hi: 'there'});
+  })
+
 
 export default socket
